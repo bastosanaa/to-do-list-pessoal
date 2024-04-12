d11ce1dd2d0ffa1d24a447a9d82e74d08bf74346
bastosanaa <bastosanaa11@gmail.com>
2024-04-11 21:42:38 -0300

    Feat: botao cancelar funcionando

--- scripts-crud.js
+++ scripts-crud.js
@@ -1,11 +1,27 @@
 const adicionarTarefaBtn = document.querySelector('.lista-de-tarefas-add-button');
 const formAddTarefa = document.querySelector('.form-adicionar-tarefa');
+const cancelarAddTarefa = document.querySelector('.form-footer__button--cancelar');
+const salvarAddTarefa = document.querySelector('.form-footer__button--cancelar')
+const formTextArea = document.querySelector('.form-textarea')
+const listaDeTarefas = document.querySelector('.lista-de-tarefas')
+
+let tarefas = JSON.parse
 
 adicionarTarefaBtn.addEventListener('click', () => {
     formAddTarefa.classList.toggle('hidden')
 })
-console.log(formAddTarefa.classList)
 
-// function fecharFormAddTarefa{
-//     formAddTarefa.classList.
-// }
+cancelarAddTarefa.addEventListener('click', fecharForm)
+
+function fecharForm(){
+    formTextArea.value = ''
+    formAddTarefa.classList.add('hidden')
+}
+
+salvarAddTarefa.addEventListener('click', addTarefa)
+
+function addTarefa() {
+    let nomeTarefa = formAddTarefa.value
+    listaDeTarefas
+    console.log(nomeTarefa)
+}
