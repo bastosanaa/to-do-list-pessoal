0719216902fc7a625a94ab7092b07881013b9998
Ana <bastosanaa11@gmail.com>
2024-04-29 09:52:23 -0300

    Feat: função de adicionar tarefas

--- scripts-crud.js
+++ scripts-crud.js
@@ -1,7 +1,7 @@
 const adicionarTarefaBtn = document.querySelector('.lista-de-tarefas-add-button');
 const formAddTarefa = document.querySelector('.form-adicionar-tarefa');
 const cancelarAddTarefa = document.querySelector('.form-footer__button--cancelar');
-const salvarAddTarefa = document.querySelector('.form-footer__button--cancelar')
+const salvarAddTarefa = document.querySelector('.form-footer__button--salvar')
 const formTextArea = document.querySelector('.form-textarea')
 const listaDeTarefas = document.querySelector('.lista-de-tarefas')
 
@@ -20,8 +20,25 @@ function fecharForm(){
 
 salvarAddTarefa.addEventListener('click', addTarefa)
 
+
+//<li> <img src="styles/imagens/check-icon.png" alt="" class="tarefa-icone">a</li>
 function addTarefa() {
-    let nomeTarefa = formAddTarefa.value
-    listaDeTarefas
-    console.log(nomeTarefa)
+    let nomeTarefa = formTextArea.value
+    const li = document.createElement('li')
+    li.classList.add('tem-tarefa')
+
+    const checkImg = document.createElement('img')
+    checkImg.setAttribute('src' , 'styles/imagens/check-icon.png')
+    checkImg.classList.add('tarefa-icone')
+
+    const descricaoTarefa = document.createElement('p')
+    descricaoTarefa.classList.add('tarefa-descricao')
+    descricaoTarefa.textContent += nomeTarefa
+
+    li.append(checkImg)
+    li.append(descricaoTarefa)
+
+    listaDeTarefas.append(li)
+    fecharForm()
 }
+
