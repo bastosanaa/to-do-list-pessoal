4aad6ff31982d56ed010e5b43874082ef0d5f0d4
Ana <bastosanaa11@gmail.com>
2024-05-03 11:10:46 -0300

    Feat: função de armazenar o tempo de realização de cada tarefa

--- scripts-crud.js
+++ scripts-crud.js
@@ -1,3 +1,4 @@
+
 const adicionarTarefaBtn = document.querySelector('.lista-de-tarefas-add-button');
 const formAddTarefa = document.querySelector('.form-adicionar-tarefa');
 const cancelarAddTarefa = document.querySelector('.form-footer__button--cancelar');
@@ -8,8 +9,10 @@ const editarListaBtn = document.querySelector('.lista-de-tarefas-edit-button')
 const tarefaItemAndamento = document.querySelector('.tarefa-andamento-item')
 const finalizarTarefaBtn = document.querySelector('.finalizar-tarefa')
 
+
 let tarefaSelecionada = null
 let tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
+let tarefaEmAndamento = JSON.parse(localStorage.getItem('tarefa_selecionada')) || {}
 
 function atualizarTarefas() {
     localStorage.setItem('tarefas', JSON.stringify(tarefas))
@@ -29,9 +32,6 @@ function apagarTarefa(tarefa){
     tarefas = tarefas.filter(item => item.descricao != descricaoItemLista.textContent);
     atualizarTarefas()
 }
-
-console.log(listaDeTarefas)
-
 
 function fecharForm(){
     formTextArea.value = ''
@@ -54,20 +54,47 @@ adicionarTarefaBtn.addEventListener('click', () => {
     // fechar modo edição caso esteja adicionando tarefa
     const excluirTarefaBtns = document.querySelectorAll('.tarefa-excluir')
     excluirTarefaBtns.forEach(btn => {
-            btn.classList.toggle('hidden')
+            btn.classList.add('hidden')
+            
     })
 })
 
 cancelarAddTarefa.addEventListener('click', fecharForm)
 
-salvarAddTarefa.addEventListener('click', () =>{
+salvarAddTarefa.addEventListener('click', () => {
     const tarefa = {
-        descricao: formTextArea.value
+        descricao: formTextArea.value,
+        tempoDecorrido : "",
+        id: formTextArea.value
     }
     tarefas.push(tarefa)
     addTarefa(tarefa)
 })
 
+// implementando
+finalizarTarefaBtn.addEventListener("click", finalizarTarefa )
+
+function finalizarTarefa(){
+    
+    const idTarefaSelecionada = JSON.parse(localStorage.getItem('tarefa_selecionada'));
+    tarefas.forEach(tarefa => {
+        if (tarefa.id == idTarefaSelecionada){
+            const liTarefa = document.getElementById(tarefa.id)
+            liTarefa.classList.add('completada')
+            console.log(liTarefa)
+            tarefa.tempoDecorrido = formataTempo(tempoDecorridoEmSegundos)
+            idTarefaSelecionada
+            
+        }
+        atualizarTarefas()
+        carregarTarefas()
+    })
+
+    pararContagem()
+    zerarContagem()
+    tarefaItemAndamento.textContent = 'nenhuma tarefa em andamento...'
+    
+}
 
 function addTarefa(tarefa) {
 
@@ -81,7 +108,11 @@ function addTarefa(tarefa) {
     
     const descricaoTarefa = document.createElement('p')
     descricaoTarefa.classList.add('tarefa-descricao')
-    descricaoTarefa.textContent += tarefa.descricao
+    descricaoTarefa.textContent = tarefa.descricao
+
+    const temporizadorTarefa = document.createElement('p')
+    temporizadorTarefa.classList.add('temporizador-tarefa')
+    temporizadorTarefa.textContent = tarefa.tempoDecorrido
 
     const excluirBtn = document.createElement('button')
     excluirBtn.classList.add('tarefa-excluir')
@@ -94,7 +125,10 @@ function addTarefa(tarefa) {
     excluirBtn.appendChild(binImg)
     li.appendChild(checkImg)
     li.appendChild(descricaoTarefa)
+    li.appendChild(temporizadorTarefa)
     li.appendChild(excluirBtn)
+    
+    li.setAttribute('id', tarefa.id)
 
     listaDeTarefas.appendChild(li)
 
@@ -105,7 +139,12 @@ function addTarefa(tarefa) {
     li.onclick = () => {
         tarefaSelecionada = tarefa
         tarefaItemAndamento.textContent = tarefaSelecionada.descricao
+        const liTarefaAndamento = document.getElementById(tarefa.id)
+        localStorage.setItem('tarefa_selecionada', JSON.stringify(liTarefaAndamento.id))
+        pararContagem()
+        zerarContagem()
     }
+
 }
 
 carregarTarefas()
--- temporizador.js
+++ temporizador.js
@@ -18,20 +18,29 @@ function pararContagem() {
     intervaloId = null
 }
 
+function zerarContagem(){
+    tempoDecorridoEmSegundos = 0;
+    playPauseIcon.setAttribute('src', '/styles/imagens/play-button.png');
+    mostrarTempo()
+}
+
 function iniciarOuPausarContagem() {
     if(!intervaloId){
         playPauseIcon.setAttribute('src', '/styles/imagens/pause-button.png')
-        intervaloId = setInterval(atualizarTempo,1000);
+        intervaloId = setInterval(atualizarTempo,100);
     } else {
         playPauseIcon.setAttribute('src', '/styles/imagens/play-button.png')
         pararContagem()
     }
+}
 
+function formataTempo(tempoDesformatado) {
+    const tempo = new Date(tempoDesformatado * 1000)
+    const tempoFormatado = tempo.toLocaleTimeString('pt-Br', {minute: '2-digit', second:'2-digit'})
+    return tempoFormatado
 }
 
 function mostrarTempo() {
-    const tempo = new Date(tempoDecorridoEmSegundos * 1000)
-    const tempoFormatado = tempo.toLocaleTimeString('pt-Br', {minute: '2-digit', second:'2-digit'})
-    tempoNaTela.innerHTML = `${tempoFormatado}`
+    tempoNaTela.innerHTML = `${formataTempo(tempoDecorridoEmSegundos)}`
 }
 mostrarTempo()
